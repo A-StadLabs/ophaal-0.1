a7d4009234ae42c32c4645bc20b04ca8f9c76972
Michael Thuy <michaelthuy@5cg426h2hl.stad.antwerpen.local>
2015-06-17 16:36:54 +0200

    yoyoyo

---
+++ public/components/expandjs/lib/tester/isPolyfilled.js
@@ -0,0 +1,28 @@
+/*jslint browser: true, devel: true, node: true, ass: true, nomen: true, unparam: true, indent: 4 */
+
+/**
+ * @license
+ * Copyright (c) 2015 The ExpandJS authors. All rights reserved.
+ * This code may only be used under the BSD style license found at https://expandjs.github.io/LICENSE.txt
+ * The complete set of authors may be found at https://expandjs.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at https://expandjs.github.io/CONTRIBUTORS.txt
+ */
+(function () {
+    "use strict";
+
+    var lodash = require('lodash');
+
+    /**
+     * Checks if `value` is a polyfilled `HTMLElement`.
+     *
+     * @function isPolyfilled
+     * @param {*} value The value to check.
+     * @returns {boolean} Returns `true` or `false` accordingly to the check.
+     */
+    module.exports = function isPolyfilled(value) {
+        if (value && lodash.has(value, 'node') && lodash.has(Object.getPrototypeOf(value), '_queryElement')) { return true; } // Shady DOM
+        if (value && (value.__impl4cf1e782hg__ || value.__wrapper8e3dd93a60__)) { return true; } // Shadow DOM
+        return false;
+    };
+
+}());
